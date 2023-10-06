311a1e1c6af958029cc26fbf2d979042367c74c1
alibukar-coder <125382835+alibukar-coder@users.noreply.github.com>
2023-10-06 11:16:16 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,36 @@
 import os
 import gradio as gr
 import numpy as np
+import pickle
+from collections import Counter
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+from pathlib import Path
 
 TITLE = "Voice-based Age Verification"
 DESP = ("Press the record button and say - I am happy or I am hungry")
 port = int(os.environ.get('PORT', 8000))
 
+def load_models():
+    """Load trained models"""
+
+    with open('svm_classifier.pkl', 'rb') as svm_file:
+        svm_model = pickle.load(svm_file)
+
+    with open('rf_classifier.pkl', 'rb') as rf_file:
+        rf_model = pickle.load(rf_file)
+
+    with open('xgb_classifier.pkl', 'rb') as xgb_file:
+        xgb_model = pickle.load(xgb_file)
+
+    print('Debug Models Loaded!!!')
+
+    return svm_model, rf_model, xgb_model
+    
 def reverse_audio(audio):
+    print('Load Models!')
+    all_models = load_models()
     sr, data = audio
     return (sr, np.flipud(data))
 
